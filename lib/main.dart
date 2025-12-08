2c0355b779d75b2644390b0e73c83d8273eaf254
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2025-12-08 11:09:04 +0530

    main.dart : splash page location change

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "SharedPreferences",
+      title: "Shared Preferences",
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
