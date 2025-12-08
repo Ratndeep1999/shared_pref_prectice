316e88744a0f1aba139675af6fac5e4d02622038
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2025-12-08 11:36:49 +0530

    label_text_widget.dart : more parameters add

--- lib/Widgets/label_text_widget.dart
+++ lib/Widgets/label_text_widget.dart
@@ -2,11 +2,29 @@ import 'package:flutter/material.dart';
 
 /// Custom Label Widget
 class LabelTextWidget extends StatelessWidget {
-  const LabelTextWidget({super.key, required this.label});
+  const LabelTextWidget({
+    super.key,
+    required this.label,
+    this.fontSize,
+    this.fontWeight,
+    this.letterSpacing,
+  });
+
+  /// Parameters
   final String label;
+  final double? fontSize;
+  final FontWeight? fontWeight;
+  final double? letterSpacing;
 
   @override
   Widget build(BuildContext context) {
-    return Text(label);
+    return Text(
+      label,
+      style: TextStyle(
+        fontSize: fontSize,
+        fontWeight: fontWeight,
+        letterSpacing: letterSpacing,
+      ),
+    );
   }
 }
