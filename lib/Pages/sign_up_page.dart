435fd0d9b681d7760797226783e8cfee97807b21
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2025-12-08 16:58:54 +0530

    sign_up_page.dart : appbar add

--- lib/Pages/sign_up_page.dart
+++ lib/Pages/sign_up_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../Widgets/label_text_widget.dart';
 
 class SignUpPage extends StatefulWidget {
   const SignUpPage({super.key});
@@ -9,5 +10,12 @@ class SignUpPage extends StatefulWidget {
 
 class SignUpPageState extends State<SignUpPage> {
   @override
-  Widget build(BuildContext context) => Scaffold();
+  Widget build(BuildContext context) => Scaffold(
+    appBar: AppBar(
+      title: LabelTextWidget(label: "SignUp Page"),
+      centerTitle: true,
+      backgroundColor: Colors.purple,
+    ),
+    body: SafeArea(child: Column(children: [])),
+  );
 }
