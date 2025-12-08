c699a8d5e0064c289b37a80c0c796868e0e809cf
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2025-12-08 12:45:00 +0530

    text_form_field_widget.dart : refactor and parrameters pass some of them can be null

--- lib/Widgets/text_form_field_widget.dart
+++ lib/Widgets/text_form_field_widget.dart
@@ -1,21 +1,42 @@
 import 'package:flutter/material.dart';
 
 class TextFormFieldWidget extends StatelessWidget {
-  const TextFormFieldWidget({super.key, required this.controller});
+  const TextFormFieldWidget({
+    super.key,
+    required this.controller,
+    this.obscureText = false,
+    this.keyboardType = TextInputType.text,
+    this.validator,
+    this.onSaved,
+    this.focusNode,
+    this.isSuffixIcon = false,
+    this.suffixIcon,
+    required this.hintText,
+  });
 
   final TextEditingController controller;
+  final bool obscureText;
+  final TextInputType keyboardType;
+  final FormFieldValidator<String?>? validator;
+  final FormFieldSetter<String>? onSaved;
+  final FocusNode? focusNode;
+  final bool isSuffixIcon;
+  final IconData? suffixIcon;
+  final String hintText;
 
   @override
   Widget build(BuildContext context) {
     return TextFormField(
       controller: controller,
-      keyboardType: TextInputType.emailAddress,
-      validator: (value) {
-        return null;
-      },
-      onSaved: (value) {
-        value = value;
-      },
+      keyboardType: keyboardType,
+      obscureText: obscureText,
+      validator: validator,
+      onSaved: onSaved,
+      focusNode: focusNode,
+      decoration: InputDecoration(
+        suffixIcon: isSuffixIcon ? Icon(suffixIcon) : null,
+        hint: Text(hintText),
+      ),
     );
   }
 }
