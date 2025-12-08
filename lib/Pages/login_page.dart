5270e980a6dcf6604a49443aa2af281c6599400a
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2025-12-08 12:05:40 +0530

    login_page.dart : email section (label and textFormField) add, controllers add, initialize inside initState and dispose inside dispose method

--- lib/Pages/login_page.dart
+++ lib/Pages/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_pref_prectice/Widgets/label_text_widget.dart';
 import '../Widgets/filled_button_widget.dart';
 
 class LoginPage extends StatefulWidget {
@@ -9,22 +10,52 @@ class LoginPage extends StatefulWidget {
 }
 
 class LoginPageState extends State<LoginPage> {
+  late final TextEditingController emailOrUsernameController;
+  late final TextEditingController passwordController;
+
+  @override
+  void initState() {
+    emailOrUsernameController = TextEditingController();
+    passwordController = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    emailOrUsernameController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
+      body: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 16.0),
+        child: Form(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              
+              /// Username or Email Section
+              LabelTextWidget(label: 'Enter EmailId or Username'),
+              TextFormField(
+                controller: emailOrUsernameController,
+                keyboardType: TextInputType.emailAddress,
+                validator: (value){return null;},
+                onSaved: (value){},
+              ),
 
 
-          /// Button
-          FilledButtonWidget(
-            buttonLabel: "Loggin...",
-            onPress: () {},
-          )
-        ],
+              SizedBox(height: 50,),
+              
+              /// Button
+              FilledButtonWidget(buttonLabel: "Loggin...", onPress: () {}),
+            ],
+          ),
+        ),
       ),
     );
   }
