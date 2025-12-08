f85f9a7c66a024cf27284a82004df34bf2176d39
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2025-12-08 16:55:46 +0530

    sign_up_page.dart : layout start

---
+++ lib/Pages/sign_up_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class SignUpPage extends StatefulWidget {
+  const SignUpPage({super.key});
+
+  @override
+  State<SignUpPage> createState() => SignUpPageState();
+}
+
+class SignUpPageState extends State<SignUpPage> {
+  @override
+  Widget build(BuildContext context) => Scaffold();
+}
