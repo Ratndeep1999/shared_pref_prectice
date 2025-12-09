16a10a6c8dfdd193cd8fd576fe424d4728b68f7d
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2025-12-09 09:19:01 +0530

    home_page.dart : logout button add with navigation method to navigate login page

--- lib/Pages/home_page.dart
+++ lib/Pages/home_page.dart
@@ -1,10 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:shared_pref_prectice/Pages/login_page.dart';
+import 'package:shared_pref_prectice/Widgets/filled_button_widget.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(),
+      body: SafeArea(
+        child: Column(
+          children: [
+            /// Logout Button
+            FilledButtonWidget(
+              buttonLabel: "Logout",
+              onPress: () => _navigateToLoginPage(context),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  /// Method to Navigate LoginPage()
+  _navigateToLoginPage(BuildContext context) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => LoginPage()),
+    );
   }
 }
