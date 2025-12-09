589b58a38d799b887bbf5712f64510d87560bf34
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2025-12-09 12:10:16 +0530

    home_page.dart: entire page add to single child scroll view

--- lib/Pages/home_page.dart
+++ lib/Pages/home_page.dart
@@ -18,38 +18,40 @@ class HomePage extends StatelessWidget {
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.all(16.0),
-          child: Column(
-            children: [
-              /// Card to Show Saved Data
-              Card(
-                elevation: 5.0,
-                margin: EdgeInsets.symmetric(horizontal: 8.0, vertical: 50.0),
-                child: Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      /// Saved Data
-                      LabelTextWidget(label: "Profile :"),
-                      const SizedBox(height: 20.0,),
-                      LabelTextWidget(label: "ratndeep"),
-                      LabelTextWidget(label: "Ratndeep Chandankhede", ),
-                      LabelTextWidget(label: "ratndeepChandankhede@gmail.com"),
-                      LabelTextWidget(label: "ratndeep@121"),
-                      LabelTextWidget(label: "+91 8558830830"),
-                    ],
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                /// Card to Show Saved Data
+                Card(
+                  elevation: 5.0,
+                  margin: EdgeInsets.symmetric(horizontal: 8.0, vertical: 50.0),
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        /// Saved Data
+                        LabelTextWidget(label: "Profile :"),
+                        const SizedBox(height: 20.0,),
+                        LabelTextWidget(label: "ratndeep"),
+                        LabelTextWidget(label: "Ratndeep Chandankhede", ),
+                        LabelTextWidget(label: "ratndeepChandankhede@gmail.com"),
+                        LabelTextWidget(label: "ratndeep@121"),
+                        LabelTextWidget(label: "+91 8558830830"),
+                      ],
+                    ),
                   ),
                 ),
-              ),
-              SizedBox(height: 50,),
-              /// Logout Button
-              Center(
-                child: FilledButtonWidget(
-                  buttonLabel: "Logout",
-                  onPress: () => _navigateToLoginPage(context),
+                SizedBox(height: 50,),
+                /// Logout Button
+                Center(
+                  child: FilledButtonWidget(
+                    buttonLabel: "Logout",
+                    onPress: () => _navigateToLoginPage(context),
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
