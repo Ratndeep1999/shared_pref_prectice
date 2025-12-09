4af89151968dbea9b0391ff49ee2d5752380b70d
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2025-12-09 09:21:02 +0530

    home_page.dart : appbar add

--- lib/Pages/home_page.dart
+++ lib/Pages/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:shared_pref_prectice/Pages/login_page.dart';
 import 'package:shared_pref_prectice/Widgets/filled_button_widget.dart';
+import 'package:shared_pref_prectice/Widgets/label_text_widget.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -8,7 +9,12 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: LabelTextWidget(label: "Home Page", fontColor: Colors.black),
+        centerTitle: true,
+        backgroundColor: Colors.purple,
+        automaticallyImplyLeading: false,
+      ),
       body: SafeArea(
         child: Column(
           children: [
