255d9acfdd4469b1438de882cee36e7c18e70daf
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2025-12-09 09:49:46 +0530

    sign_up_page.dart : custom phone number field add.

--- lib/Pages/sign_up_page.dart
+++ lib/Pages/sign_up_page.dart
@@ -3,6 +3,7 @@ import 'package:shared_pref_prectice/Pages/home_page.dart';
 import 'package:shared_pref_prectice/Widgets/filled_button_widget.dart';
 import 'package:shared_pref_prectice/Widgets/text_form_field_widget.dart';
 import '../Widgets/label_text_widget.dart';
+import '../Widgets/phone_number_field-widget..dart';
 
 class SignUpPage extends StatefulWidget {
   const SignUpPage({super.key});
@@ -73,83 +74,91 @@ class SignUpPageState extends State<SignUpPage> {
   }
 
   @override
-  Widget build(BuildContext context) => Scaffold(
-    appBar: AppBar(
-      title: LabelTextWidget(label: "SignUp Page", fontColor: Colors.black),
-      centerTitle: true,
-      backgroundColor: Colors.purple,
-    ),
-    body: SafeArea(
-      child: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 32, vertical: 32),
-        child: AutofillGroup(
-          child: Form(
-            key: _formKey,
-            child: SingleChildScrollView(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  /// Full Name Section
-                  LabelTextWidget(label: "Enter Full Name", fontSize: 15),
-                  TextFormFieldWidget(
-                    controller: _fullNameController,
-                    hintText: "Enter Your Full Name",
-                  ),
-                  const SizedBox(height: 20),
+  Widget build(BuildContext context) =>
+      Scaffold(
+        appBar: AppBar(
+          title: LabelTextWidget(label: "SignUp Page", fontColor: Colors.black),
+          centerTitle: true,
+          backgroundColor: Colors.purple,
+        ),
+        body: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 32, vertical: 32),
+            child: AutofillGroup(
+              child: Form(
+                key: _formKey,
+                child: SingleChildScrollView(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
 
-                  /// Email Section
-                  LabelTextWidget(label: "Enter Email Address", fontSize: 15),
-                  TextFormFieldWidget(
-                    controller: _emailController,
-                    hintText: "Enter Your Email Address",
-                  ),
-                  const SizedBox(height: 20),
+                      /// Full Name Section
+                      LabelTextWidget(label: "Enter Full Name", fontSize: 15),
+                      TextFormFieldWidget(
+                        controller: _fullNameController,
+                        hintText: "Enter Your Full Name",
+                      ),
+                      const SizedBox(height: 20),
 
-                  /// User name Section
-                  LabelTextWidget(label: "Enter User Name", fontSize: 15),
-                  TextFormFieldWidget(
-                    controller: _userNameController,
-                    hintText: "Enter Your User Name",
-                  ),
-                  const SizedBox(height: 20),
+                      /// Email Section
+                      LabelTextWidget(
+                          label: "Enter Email Address", fontSize: 15),
+                      TextFormFieldWidget(
+                        controller: _emailController,
+                        hintText: "Enter Your Email Address",
+                      ),
+                      const SizedBox(height: 20),
 
-                  /// Password Section
-                  LabelTextWidget(label: "Enter Password", fontSize: 15),
-                  TextFormFieldWidget(
-                    controller: _passwordController,
-                    hintText: "Enter Your Password",
-                  ),
-                  const SizedBox(height: 20),
+                      /// User name Section
+                      LabelTextWidget(label: "Enter User Name", fontSize: 15),
+                      TextFormFieldWidget(
+                        controller: _userNameController,
+                        hintText: "Enter Your User Name",
+                      ),
+                      const SizedBox(height: 20),
 
-                  /// Conform Password Section
-                  LabelTextWidget(
-                    label: "Enter Conform Password",
-                    fontSize: 15,
-                  ),
-                  TextFormFieldWidget(
-                    controller: _confPasswordController,
-                    hintText: "Enter Your Conform Password",
-                  ),
-                  const SizedBox(height: 20),
+                      /// Password Section
+                      LabelTextWidget(label: "Enter Password", fontSize: 15),
+                      TextFormFieldWidget(
+                        controller: _passwordController,
+                        hintText: "Enter Your Password",
+                      ),
+                      const SizedBox(height: 20),
 
-                  /// Mobile No. Section
-                  const SizedBox(height: 50),
+                      /// Conform Password Section
+                      LabelTextWidget(
+                        label: "Enter Conform Password",
+                        fontSize: 15,
+                      ),
+                      TextFormFieldWidget(
+                        controller: _confPasswordController,
+                        hintText: "Enter Your Conform Password",
+                      ),
+                      const SizedBox(height: 20),
 
-                  /// Button
-                  Center(
-                    child: FilledButtonWidget(
-                      buttonLabel: "Create Account",
-                      onPress: _navigateToHomePage(),
-                    ),
+                      /// Mobile No. Section
+                      LabelTextWidget(
+                        label: "Enter Conform Password",
+                        fontSize: 15,
+                      ),
+                      PhoneNumberFieldWidget(),
+                      const SizedBox(height: 50),
+
+                      /// Button
+                      Center(
+                        child: FilledButtonWidget(
+                          buttonLabel: "Create Account",
+                          onPress: () => _navigateToHomePage(),
+                        ),
+                      ),
+                    ],
                   ),
-                ],
+                ),
               ),
             ),
           ),
         ),
-      ),
-    ),
-  );
+      );
 
   /// Method to navigate HomePage
   _navigateToHomePage() {
@@ -159,3 +168,5 @@ class SignUpPageState extends State<SignUpPage> {
     );
   }
 }
+
+
