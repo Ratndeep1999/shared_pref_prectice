1cc9abb5b37694e6c628bba17007e053d39827ef
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2025-12-10 09:52:00 +0530

    main.dart : move MyApp class in separate file as App() to maintain readability

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:shared_pref_prectice/Pages/splash_page.dart';
+import 'core/app/app.dart';
 
-/// Entry point of the program
-main() {
-  runApp(const MyApp());
-}
-
-/// MyApp class (immutable)
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "Shared Preferences",
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      /// Page tha appear first
-      home: SplashPage(),
-    );
-  }
+void main() {
+  runApp(const App());
 }
