0fe579595f53a5b2477b723d7ee5e669d17ca97e
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2025-12-10 14:47:19 +0530

    app_strings.dart : app strings add

---
+++ lib/core/constants/app_strings.dart
@@ -0,0 +1,27 @@
+class AppStrings {
+  // General
+  static const appName = "Shared Pref Practice";
+
+  // Splash
+  static const splashTitle = "Splash Page...";
+
+  // Login
+  static const loginTitle = "Login Page";
+  static const emailOrUsername = "Enter EmailId or Username";
+  static const password = "Enter Password";
+  static const loginButton = "Login...";
+  static const createAccount = "Don't Have Account? Create Account";
+
+  // Signup
+  static const signupTitle = "SignUp Page";
+  static const fullName = "Enter Full Name";
+  static const email = "Enter Email Address";
+  static const username = "Enter User Name";
+  static const confirmPassword = "Enter Confirm Password";
+  static const phone = "Enter Phone Number";
+  static const createButton = "Create Account";
+
+  // Home
+  static const homeTitle = "Home Page";
+  static const logout = "Logout";
+}
