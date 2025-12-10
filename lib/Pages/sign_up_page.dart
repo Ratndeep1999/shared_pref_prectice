9e32c259963fb60f90e109305f7f2dbb703478ea
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2025-12-10 15:27:55 +0530

    sign_up_page.dart app_strings and fields add

--- lib/Pages/sign_up_page.dart
+++ lib/Pages/sign_up_page.dart
@@ -2,9 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:intl_phone_field/phone_number.dart';
 import 'package:shared_pref_prectice/Widgets/filled_button_widget.dart';
 import 'package:shared_pref_prectice/Widgets/text_form_field_widget.dart';
+import 'package:shared_pref_prectice/core/constants/app_strings.dart';
 import 'package:shared_pref_prectice/core/utils/validators.dart';
 import '../Widgets/label_text_widget.dart';
 import '../Widgets/phone_number_field-widget..dart';
+import '../core/helpers/form_fields.dart';
 
 class SignUpPage extends StatefulWidget {
   const SignUpPage({super.key});
@@ -14,23 +16,8 @@ class SignUpPage extends StatefulWidget {
 }
 
 class SignUpPageState extends State<SignUpPage> {
-  /// Controllers
-  late final TextEditingController _fullNameController;
-  late final TextEditingController _emailController;
-  late final TextEditingController _userNameController;
-  late final TextEditingController _passwordController;
-  late final TextEditingController _confPasswordController;
-  late final TextEditingController _mobileNoController;
-
-  /// Focus Nodes
-  late final FocusNode _fullNameNode;
-  late final FocusNode _emailNode;
-  late final FocusNode _usernameNode;
-  late final FocusNode _passwordNode;
-  late final FocusNode _confPasswordNode;
-  late final FocusNode _mobileNoNode;
-
   /// Parameters
+  late final FormFields fields;
   late String _fullName;
   late String _email;
   late String _userName;
@@ -44,42 +31,23 @@ class SignUpPageState extends State<SignUpPage> {
 
   @override
   void initState() {
-    _fullNameController = TextEditingController();
-    _emailController = TextEditingController();
-    _userNameController = TextEditingController();
-    _passwordController = TextEditingController();
-    _confPasswordController = TextEditingController();
-    _mobileNoController = TextEditingController();
-    _fullNameNode = FocusNode();
-    _emailNode = FocusNode();
-    _usernameNode = FocusNode();
-    _passwordNode = FocusNode();
-    _confPasswordNode = FocusNode();
-    _mobileNoNode = FocusNode();
+    fields = FormFields();
     super.initState();
   }
 
   @override
   void dispose() {
-    _fullNameController.dispose();
-    _emailController.dispose();
-    _userNameController.dispose();
-    _passwordController.dispose();
-    _confPasswordController.dispose();
-    _mobileNoController.dispose();
-    _fullNameNode.dispose();
-    _emailNode.dispose();
-    _usernameNode.dispose();
-    _passwordNode.dispose();
-    _confPasswordNode.dispose();
-    _mobileNoNode.dispose();
+    fields.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) => Scaffold(
     appBar: AppBar(
-      title: LabelTextWidget(label: "SignUp Page", fontColor: Colors.black),
+      title: LabelTextWidget(
+        label: AppStrings.signupTitle,
+        fontColor: Colors.black,
+      ),
       centerTitle: true,
       backgroundColor: Colors.purple,
     ),
@@ -94,13 +62,13 @@ class SignUpPageState extends State<SignUpPage> {
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   /// Full Name Section
-                  LabelTextWidget(label: "Enter Full Name", fontSize: 15),
+                  LabelTextWidget(label: AppStrings.fullName, fontSize: 15),
                   TextFormFieldWidget(
-                    controller: _fullNameController,
-                    hintText: "Enter Your Full Name",
+                    controller: fields.fullNameController,
+                    hintText: AppStrings.yourFullName,
                     keyboardType: TextInputType.name,
-                    focusNode: _fullNameNode,
-                    nextFocus: _emailNode,
+                    focusNode: fields.fullNameNode,
+                    nextFocus: fields.emailNode,
                     autoFocus: true,
                     validator: Validators.fullName,
                     onSaved: (fName) => _fullName = fName!.trim().toLowerCase(),
@@ -110,37 +78,37 @@ class SignUpPageState extends State<SignUpPage> {
                   /// Email Section
                   LabelTextWidget(label: "Enter Email Address", fontSize: 15),
                   TextFormFieldWidget(
-                    controller: _emailController,
+                    controller: fields.emailController,
                     hintText: "Enter Your Email Address",
                     keyboardType: TextInputType.emailAddress,
-                    focusNode: _emailNode,
-                    nextFocus: _usernameNode,
+                    focusNode: fields.emailNode,
+                    nextFocus: fields.usernameNode,
                     validator: Validators.email,
                     onSaved: (email) => _email = email!.trim().toLowerCase(),
                   ),
                   const SizedBox(height: 20),
 
                   /// User name Section
-                  LabelTextWidget(label: "Enter User Name", fontSize: 15),
+                  LabelTextWidget(label: AppStrings.username, fontSize: 15),
                   TextFormFieldWidget(
-                    controller: _userNameController,
-                    hintText: "Enter Your User Name",
+                    controller: fields.usernameController,
+                    hintText: AppStrings.yourUsername,
                     keyboardType: TextInputType.name,
-                    focusNode: _usernameNode,
-                    nextFocus: _passwordNode,
+                    focusNode: fields.usernameNode,
+                    nextFocus: fields.passwordNode,
                     validator: Validators.username,
                     onSaved: (uName) => _userName = uName!.trim().toLowerCase(),
                   ),
                   const SizedBox(height: 20),
 
                   /// Password Section
-                  LabelTextWidget(label: "Enter Password", fontSize: 15),
+                  LabelTextWidget(label: AppStrings.password, fontSize: 15),
                   TextFormFieldWidget(
-                    controller: _passwordController,
-                    hintText: "Enter Your Password",
+                    controller: fields.passwordController,
+                    hintText: AppStrings.yourPassword,
                     keyboardType: TextInputType.visiblePassword,
-                    focusNode: _passwordNode,
-                    nextFocus: _confPasswordNode,
+                    focusNode: fields.passwordNode,
+                    nextFocus: fields.confPasswordNode,
                     obscureText: !isPasswordVisible,
                     isSuffixIcon: true,
                     suffixIcon: isPasswordVisible
@@ -154,15 +122,15 @@ class SignUpPageState extends State<SignUpPage> {
 
                   /// Conform Password Section
                   LabelTextWidget(
-                    label: "Enter Conform Password",
+                    label: AppStrings.confirmPassword,
                     fontSize: 15,
                   ),
                   TextFormFieldWidget(
-                    controller: _confPasswordController,
-                    hintText: "Enter Your Conform Password",
+                    controller: fields.confPasswordController,
+                    hintText: AppStrings.passwordAgain,
                     keyboardType: TextInputType.visiblePassword,
-                    focusNode: _confPasswordNode,
-                    nextFocus: _mobileNoNode,
+                    focusNode: fields.confPasswordNode,
+                    nextFocus: fields.phoneNode,
                     isSuffixIcon: true,
                     suffixIcon: _isPassConfPassSame
                         ? Icons.check_circle
@@ -171,23 +139,27 @@ class SignUpPageState extends State<SignUpPage> {
                         ? Color(0xFF93c743)
                         : Color(0xFFFF4C4C),
                     onChanged: _onChangedConfPassword,
-                    validator: (value) => Validators.confirmPassword(value, _passwordController.text),
+                    validator: (value) => Validators.confirmPassword(
+                      value,
+                      fields.passwordController.text,
+                    ),
                   ),
                   const SizedBox(height: 20),
 
                   /// Mobile No. Section
-                  LabelTextWidget(label: "Enter Phone Number", fontSize: 15),
+                  LabelTextWidget(label: AppStrings.phone, fontSize: 15),
                   PhoneNumberFieldWidget(
-                    controller: _mobileNoController,
-                    focusNode: _mobileNoNode,
-                    onSaved: (PhoneNumber? num) => _mobileNo = "${num!.countryCode} ${num.number}",
+                    controller: fields.phoneController,
+                    focusNode: fields.phoneNode,
+                    onSaved: (PhoneNumber? num) =>
+                        _mobileNo = "${num!.countryCode} ${num.number}",
                   ),
                   const SizedBox(height: 20),
 
                   /// Signup Button
                   Center(
                     child: FilledButtonWidget(
-                      buttonLabel: "Create Account",
+                      buttonLabel: AppStrings.createButton,
                       isClicked: _isSigning,
                       onPress: _isFormValid ? signupPress : () {},
                     ),
@@ -207,7 +179,8 @@ class SignUpPageState extends State<SignUpPage> {
     if (!_formKey.currentState!.validate()) return;
 
     // Check password and confPassword
-    if (_passwordController.text != _confPasswordController.text) return;
+    if (fields.passwordController.text != fields.confPasswordController.text)
+      return;
 
     _formKey.currentState!.save();
     debugPrint(_fullName);
@@ -240,7 +213,7 @@ class SignUpPageState extends State<SignUpPage> {
   /// Check Password and Conf-Password same or Not and Validate Form
   void _onChangedConfPassword(String confPassword) {
     setState(() {
-      _isPassConfPassSame = (_passwordController.text == confPassword)
+      _isPassConfPassSame = (fields.passwordController.text == confPassword)
           ? true
           : false;
     });
@@ -255,5 +228,4 @@ class SignUpPageState extends State<SignUpPage> {
       });
     }
   }
-
 }
